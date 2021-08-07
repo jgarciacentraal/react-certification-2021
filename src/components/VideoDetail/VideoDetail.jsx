6b9c9b0f0543e707a61bb6dfdc836aaf1c06e4a5
Julio Estefanes <jsgr1992@gmail.com>
2021-08-06 23:03:28 -0500

    some styles

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 
-import { 
+import {
   VideoContainer,
   VideoInfoContainer,
   VideoTitle,
   VideoText,
-  VideoiFrame } from './Styles';
+  VideoiFrame,
+} from './Styles';
 
 export default function VideoDetail({ videoDetail }) {
-
   const { videoId } = videoDetail.id;
   const { title, description } = videoDetail.snippet;
-  
+
   return (
     <>
       <VideoContainer>
-        <VideoiFrame title="playVideo" src={`https://www.youtube.com/embed/${videoId}`} /> 
-      <VideoInfoContainer>
-        <VideoTitle data-testid={title}>{title}</VideoTitle>
-        <VideoText>{description}</VideoText>
-      </VideoInfoContainer>
-    </VideoContainer>
+        <VideoiFrame title="playVideo" src={`https://www.youtube.com/embed/${videoId}`} />
+        <VideoInfoContainer>
+          <VideoTitle data-testid={title}>{title}</VideoTitle>
+          <VideoText>{description}</VideoText>
+        </VideoInfoContainer>
+      </VideoContainer>
     </>
   );
 }
-
-
--- src/pages/Home/Styles.js
+++ src/pages/Home/Styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const HomeContent = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto;
-  box-sizing: border-box;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
 `;
